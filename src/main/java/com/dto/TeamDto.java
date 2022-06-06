f16035f050d95373dcc65bca85d24c5f9e7ec498
Teethu <teethu@vipicash.com>
2022-06-06 23:21:52 +0200

    Update TeamDto.java

--- src/main/java/com/dto/TeamDto.java
+++ src/main/java/com/dto/TeamDto.java
@@ -1,36 +1,46 @@
 package com.dto;
 
+/**
+ * A DTO class for the team
+ */
 
 public class TeamDto {
 
-    private String homeTeam;
+	private String homeTeam;
 	private String awayTeam;
-    private int homeTeamScore;
-    private int awayTeamScore;
-    
+	private int homeTeamScore;
+	private int awayTeamScore;
+
 	public String getHomeTeam() {
 		return homeTeam;
 	}
+
 	public void setHomeTeam(String homeTeam) {
 		this.homeTeam = homeTeam;
 	}
+
 	public String getAwayTeam() {
 		return awayTeam;
 	}
+
 	public void setAwayTeam(String awayTeam) {
 		this.awayTeam = awayTeam;
 	}
+
 	public int getHomeTeamScore() {
 		return homeTeamScore;
 	}
+
 	public void setHomeTeamScore(int homeTeamScore) {
 		this.homeTeamScore = homeTeamScore;
 	}
+
 	public int getAwayTeamScore() {
 		return awayTeamScore;
 	}
+
 	public void setAwayTeamScore(int awayTeamScore) {
 		this.awayTeamScore = awayTeamScore;
 	}
-	
+
 }
